5f4423407949f1be58b83ca9163eaee708b595c7
Kalaimagal18 <kalaimagalrocketscience@gmail.com>
2024-10-14 23:18:44 +0530

    low2.c
    Switch case for days

---
+++ Low2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main();
+{
+  int a;
+  scanf("%d",&a);
+ switch(a)
+{
+case 1:
+printf("sunday");
+break;
+
+case 2:
+printf("monday");
+break;
+
+case 3:
+printf("tuesday");
+break;
+
+case 4:
+printf("Wednesday");
+break;
+
+case 5:
+printf("Thursday");
+break;
+
+case 6:
+printf("friday");
+break;
+
+case 7:
+printf("Saturday");
+break;
+
+default:
+printf("invalid");
+break;
+}
+}
+  
